4780cc7372dda83f4340cffc2cc0bff48c0f39ce
Jasper Woudenberg <mail@jasperwoudenberg.com>
2024-12-23 10:33:55 +0100

    Get tree sitter via flake instead of build.zig.zon
    There's a couple of reasons for this:
    
    - I need the 'highlight' sub-project for Jay, which is a rust project.
      This means I have to give up on Zig compiling everything and easy
      cross-compilation, unfortunately. I incorporated the cargo build
      command into build.zig, but it runs cargo every time resulting in a
      much slower 'zig build' experience. Putting that compilation in
      flake.nix means I need to run it only once after changing the
      tree-sitter dependency. That cargo compilation will also be covered by
      my existing caching setup in CI.
    
    - I'll also need the tree-sitter CLI, to compile grammars for languages.
      Getting tree-sitter via Nix means I can easily ensure my use of the
      tree-sitter CLI, highlight tool, and tree-sitter linked library will
      all be the same version.

--- flake.nix
+++ flake.nix
@@ -4,27 +4,46 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     roc.url = "github:roc-lang/roc";
+    naersk.url = "github:nix-community/naersk";
+    tree-sitter.url = "github:tree-sitter/tree-sitter/v0.24.5";
+    tree-sitter.flake = false;
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      roc,
-    }:
-    {
-      devShell."x86_64-linux" =
-        let
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
-        in
-        pkgs.mkShell {
-          packages = [
-            roc.packages."x86_64-linux".cli
-            pkgs.cargo # for building tree-sitter-highlight
-            pkgs.entr # for watch.sh
-            pkgs.zig
-            pkgs.zls
-          ];
+  outputs = inputs: {
+    devShell."x86_64-linux" =
+      let
+        pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+        naersk = pkgs.callPackage inputs.naersk { };
+
+        tree-sitter = naersk.buildPackage {
+          src = "${inputs.tree-sitter}";
+          postInstall = ''
+            PREFIX=$out make install
+          '';
         };
-    };
+
+        highlight = naersk.buildPackage {
+          src = "${inputs.tree-sitter}";
+          cargoBuildOptions = defaults: defaults ++ [ "--package=tree-sitter-highlight" ];
+          postInstall = ''
+            install -D -m644 target/release/libtree_sitter_highlight.a $out/lib/libtree_sitter_highlight.a
+            install -D -m644 $src/highlight/include/tree_sitter/highlight.h $out/include/tree_sitter/highlight.h
+          '';
+        };
+      in
+      pkgs.mkShell {
+        packages = [
+          inputs.roc.packages."x86_64-linux".cli
+          pkgs.entr # for watch.sh
+          pkgs.zig
+          pkgs.zls
+          tree-sitter
+          highlight
+        ];
+
+        # Paths for build.zig to find these dependencies.
+        TREE_SITTER_PATH = "${tree-sitter}";
+        HIGHLIGHT_PATH = "${highlight}";
+      };
+  };
 }
